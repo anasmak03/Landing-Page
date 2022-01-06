446af07f96b40d360af01e13bdd6f240e2dc2af7
anasmak03 <anas11.makhloufi@gmail.com>
2022-01-07 00:55:02 +0100

    Landing Page

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger')
+
+const navmenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", () =>{
+    navmenu.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
